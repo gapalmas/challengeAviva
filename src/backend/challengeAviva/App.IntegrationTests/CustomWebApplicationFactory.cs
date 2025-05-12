704602d2015cb11b1cdeb289b8cce87d3cfa45d9
gapalmass <gapalmasolano@gmail.com>
2025-05-11 22:35:01 -0600

    Added IntegrationTests

---
+++ src/backend/challengeAviva/App.IntegrationTests/CustomWebApplicationFactory.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Extensions.Hosting;
+
+namespace App.IntegrationTests
+{
+    public class CustomWebApplicationFactory<TStartup> : WebApplicationFactory<TStartup> where TStartup : class
+    {
+        protected override IHost CreateHost(IHostBuilder builder)
+        {
+            // Si deseas reemplazar servicios para pruebas (por ejemplo mocks), puedes hacerlo aquí
+            builder.ConfigureServices(services =>
+            {
+                // Ejemplo: reemplazar servicios reales por fakes si se desea
+            });
+
+            return base.CreateHost(builder);
+        }
+    }
+}
